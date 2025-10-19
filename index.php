ee9683ee3bb7e420fd9c2fc54b9a7efa814f09db
Riddle <an7r1ddl3@gmail.com>
2025-10-20 03:18:05 +0530

    Arrays
    I'm starting to really hate this language because of how over complicated it is after using languages like python or javascript

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $name = "Tom";
 $fullName = "Tom Riddle";
 $age = 18;
+$movies = ["The Matrix", "Inception", "Interstellar", "The Dark Knight", "Fight Club"];
 
 echo "Name: $name - ";
 echo "Full Name: $fullName - ";
@@ -20,3 +21,9 @@ function trueOrFalse($value) {
 
 <h1>Welcome, <?php echo $fullName ?></h1>
 <p>Are you an adult? <?php echo trueOrFalse($age); ?></p>
+<h1>Your Favorite Movies:</h1>
+<ul>
+    <?php foreach ($movies as $movie): ?>
+        <li><?php echo $movie; ?></li>
+    <?php endforeach; ?>
+</ul>
