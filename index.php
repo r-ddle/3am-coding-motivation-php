4c975fdec6f8112edc66735f8d81fce4e1da3802
Riddle <an7r1ddl3@gmail.com>
2025-10-20 03:15:31 +0530

    If/else statements
    Nothing much to comment, pretty basic stuff

--- index.php
+++ index.php
@@ -8,6 +8,15 @@ echo "Name: $name - ";
 echo "Full Name: $fullName - ";
 echo "Age: $age ";
 
+function trueOrFalse($value) {
+    if ($value < 18) {
+        return "false";
+    } else {
+        return "true";
+    }
+}
+
 ?>
 
 <h1>Welcome, <?php echo $fullName ?></h1>
+<p>Are you an adult? <?php echo trueOrFalse($age); ?></p>
